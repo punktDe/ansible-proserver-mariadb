18fdca267bb410d397a211d11f6f20b8a74b429b
Wolfgang Zenker <zenker@punkt.de>
2020-06-19 09:32:18 +0200

    TASK: add script for waiting on cluster

---
+++ templates/has_joined_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Skript um einen Test solange auszuführen, bis erfolgreich oder timeout
+
+# Konfig
+# ------
+# maximale Wartezeit in Sekunden
+maxwait={{ mariadb.galera.join.max_wait }}
+# Pause zwischen 2 Versuchen in Sekunden
+pausetime={{ mariadb.galera.join.check_pause }}
+
+starttime=`date +%s`
+endtime=`expr $starttime + $maxwait`
+
+while [ `date +%s` -lt $endtime ]
+do
+        if echo "SHOW STATUS LIKE 'wsrep_ready';" | mysql -u root | grep 'ON'
+        then
+                exit 0
+        fi
+        if [ $pausetime -ge 1 ]
+        then
+                sleep $pausetime
+        fi
+done
+
+exit 1
