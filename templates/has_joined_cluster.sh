6b3952edf64fc2cd0a815ec9d1cfa04816405f30
Lars Liedtke <liedtke@punkt.de>
2020-06-22 09:45:25 +0200

    TASK: transfer some comments to variable names and fix indentation

--- templates/has_joined_cluster.sh
+++ templates/has_joined_cluster.sh
@@ -4,24 +4,23 @@
 
 # Config
 # ------
-# maximale Wartezeit in Sekunden
-maxwait={{ mariadb.galera.join.max_wait }}
+total_maxwait_seconds={{ mariadb.galera.join.max_wait }}
 # Pause zwischen 2 Versuchen in Sekunden
-pausetime={{ mariadb.galera.join.check_pause }}
+sleeptime_seconds={{ mariadb.galera.join.check_pause }}
 
 starttime=`date +%s`
-endtime=`expr $starttime + $maxwait`
+endtime=`expr $starttime + $total_maxwait_seconds`
 
 while [ `date +%s` -lt $endtime ]
 do
-        if echo "SHOW STATUS LIKE 'wsrep_ready';" | mysql | grep 'ON'
-        then
-                exit 0
-        fi
-        if [ $pausetime -ge 1 ]
-        then
-                sleep $pausetime
-        fi
+    if echo "SHOW STATUS LIKE 'wsrep_ready';" | mysql | grep 'ON'
+    then
+            exit 0
+    fi
+    if [ $sleeptime_seconds -ge 1 ]
+    then
+            sleep $sleeptime_sleeptime_seconds
+    fi
 done
 
 exit 1
